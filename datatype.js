bd871f3c54eea21da7dc825d76b9d71809dfffff
surya9489 <suryaprakashsp9489@gmail.com>
2025-10-11 13:00:55 +0530

    first commit

---
+++ datatype.js
@@ -0,0 +1,45 @@
+// 1. Variable Declarations
+
+var name = "Surya prakash";       // string 
+let age = 21;             // number 
+const isStudent = false;   // boolean
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+
+// 2.  Data Types
+
+let job = null;           // null value
+let salary;               // undefined 
+
+// 3. Arithmetic Operators
+
+let x = 10;
+let y = 3;
+
+let sum = x + y;          // addition
+let difference = x - y;   // subtraction
+let product = x * y;      // multiplication
+let quotient = x / y;     // division
+let remainder = x % y;    // modulus
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
+
+// 4. Comparison Operators
+
+console.log(x == "10");   // true
+console.log(x === "10");  // false 
+
+// 5. Logical Operators
+
+let a = true;
+let b = false;
+
+console.log(a && b);      // false (AND)
+console.log(a || b);      // true (OR)
